a30e9781fe1cbcdc45c11d970b5c233a34a1c75e
Abel Acuna <abel3121@gmail.com>
2023-10-01 10:58:15 -0500

    m

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -108,9 +108,9 @@ export class MessageService {
   }
 
   validateMessage(message: any){
-    console.log(message.entry[0]);
-    console.log(message.entry[0].messages);
-    const messageInfo = message.entry[0].messages[0];
+    console.log("aqui entry",message.entry[0]);
+    console.log("aqui changes",message.entry[0].changes);
+    const messageInfo = message?.entry[0]?.messages[0];
     console.log(messageInfo);
     if(messageInfo && messageInfo.from === 'me')
     {
