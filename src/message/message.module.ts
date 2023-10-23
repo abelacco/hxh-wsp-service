7f91b01caf4a8fcb596011746c91f14d519594a8
cbenz <contactocbenitez@gmail.com>
2023-10-23 10:09:36 -0300

    add doctor service to message module

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,12 +4,11 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { Message, MessageSchema } from './entities/message.entity';
 import { BotResponseService } from './bot-response/bot-response.service';
-import { WspService } from 'src/wsp/wsp.service';
-import { WspModule } from 'src/wsp/wsp.module';
+import { DoctorService } from 'src/doctor/doctor.service';
 
 @Module({
   controllers: [MessageController],
-  providers: [MessageService, BotResponseService],
+  providers: [MessageService, BotResponseService, DoctorService],
   imports: [
     MongooseModule.forFeature([
       {
