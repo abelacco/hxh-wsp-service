fe9c406f89f189cd886bf2e7426aa0b00b83de8e
Cristianbenz <contactocbenitez@gmail.com>
2023-11-02 11:33:51 -0300

    fix error handler messages returned

--- src/message/helpers/messageErrorHandler.ts
+++ src/message/helpers/messageErrorHandler.ts
@@ -1,29 +1,25 @@
-import { STEPS } from "src/config/constants";
-import { Templates } from "./templates/textTemplate";
-import { Message } from "../entities/message.entity";
+import { STEPS } from 'src/config/constants';
+import { Templates } from './templates/textTemplate';
+import { Message } from '../entities/message.entity';
 
 export const messageErrorHandler = (message: Message) => {
-    const step = message.step;
-    const patientPhone = message.phone;
-    const responses: Array<any> = [Templates.defaultMessageTemplate(patientPhone)];
-    switch (step) {
-        case STEPS.SELECT_SPECIALTY:
-            responses.push(Templates.generateSpecialitiesList(patientPhone));
-            break;
-            case STEPS.INSERT_DATE:
-            responses.push(Templates.dateStepTemplateMessage(patientPhone));
-            break;
-            case STEPS.SELECT_DOCTOR:
-            responses.push(Templates.defaultMessageTemplate(patientPhone));
-            break;
-            case STEPS.SELECT_PAYMENT:
-            responses.push(Templates.generatePaymentOptions(patientPhone));
-            break;
-            case STEPS.SUBMIT_VOUCHER:
-            responses.push(Templates.defaultMessageTemplate(patientPhone));
-            break;
-            default:
-            return responses;
-    }
-    return responses;
-}
+  const step = message.step;
+  const patientPhone = message.phone;
+  const responses: Array<any> = [
+    Templates.defaultMessageTemplate(patientPhone),
+  ];
+  switch (step) {
+    case STEPS.SELECT_SPECIALTY:
+      responses.push(Templates.generateSpecialitiesList(patientPhone));
+      break;
+    case STEPS.INSERT_DATE:
+      responses.push(Templates.dateStepTemplateMessage(patientPhone));
+      break;
+    case STEPS.SELECT_PAYMENT:
+      responses.push(Templates.generatePaymentOptions(patientPhone));
+      break;
+    default:
+      return responses;
+  }
+  return responses;
+};
