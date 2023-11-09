68f01a5629b1e91d6ea9b073d067585208d9597a
Cristianbenz <contactocbenitez@gmail.com>
2023-11-09 15:10:25 -0300

    add log whe search message by appointment aid

--- src/wsp/wsp.service.ts
+++ src/wsp/wsp.service.ts
@@ -43,10 +43,10 @@ export class WspService {
   }
 
   async updateStatus(paymentStatusDto: PaymentStatusDto) {
-    console.log("paymentStatus received", paymentStatusDto)
     const id = paymentStatusDto.id;
     const status = paymentStatusDto.status;
     const message = await this.msgService.findByAppointmentId(id);
+    console.log("message by appontment id", message)
     message.status = status.toString();
     await this.msgService.updateMessage(message.id, message);
     const templates = await this.msgService.createStatusNotification(message);
