550b4c31d022570a31d175d0b3cf28a955231a8a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-15 11:21:30 -0600

    (wsp.module) Add the marketer-bot.middleware to the wsp controller routes and add the MarketerModule to the array imports

--- src/wsp/wsp.module.ts
+++ src/wsp/wsp.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { WspService } from './wsp.service';
 import { WspController } from './wsp.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Message, MessageSchema } from 'src/message/entities/message.entity';
 import { MessageModule } from 'src/message/message.module';
 import { DoctorModule } from 'src/doctor/doctor.module';
+import { MarketerBotMiddleware } from 'src/middlewares/marketer-bot.middleware';
+import { MarketerModule } from 'src/marketer/marketer.module';
 
 @Module({
   controllers: [WspController],
@@ -12,8 +14,13 @@ import { DoctorModule } from 'src/doctor/doctor.module';
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
     MessageModule,
     DoctorModule,
+    MarketerModule
   ],
   providers: [WspService],
   exports: [WspService]
 })
-export class WspModule {}
+export class WspModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MarketerBotMiddleware).forRoutes('/');
+  }
+}
