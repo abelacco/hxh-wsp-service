373d61ab2bad33820adec30e7e5bfab0390908aa
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-20 16:32:34 -0600

    (marketer.handler) Remove the EventEmitter uses in the file and inject the MarketerEvents dependency

--- src/marketer/handler/marketer.handler.ts
+++ src/marketer/handler/marketer.handler.ts
@@ -1,91 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { validateDNI, validateRUC } from '../utils/validation';
-import { EventEmitter2 } from '@nestjs/event-emitter';
-import { Event } from '../enums/event.enum';
 import { WspMessage } from 'src/provider/types/wsp-message.interface';
+import { MarketerEvents } from '../events/marketer.event';
 
 @Injectable()
 export class MarketerHandler {
     
     constructor(
-        private eventEmitter: EventEmitter2
+        private readonly marketerEvents: MarketerEvents
     ) {}
 
-    public handleTextMessage(req: Request, data: WspMessage) {
-        console.log('handler MARKETER_TEXT_MESSAGE');
+    public async handleTextMessage(req: Request, data: WspMessage) {
+        console.log('handler MARKETER_MESSAGE');
 
-        let message = data.message.text.body;
-        // let message = req.marketerField || 'bot';
-
-        if (validateRUC(message) || validateDNI(message)) {
-            message = 'RUC/DNI';
-        }
+        let message = req.marketerField;
 
         switch (message) {
             case 'bot':
-                this.eventEmitter.emit(Event.MARKETER_START, data);
+                await this.marketerEvents.sendWelcome(data);
                 break;
-            // TODO: Probar estos case en base a los campos del registro marketer
-            // case 'DNI':
-            //     this.eventEmitter.emit(Event.MARKETER_REQUEST_RUC_DNI);
-            //     break;
-            // case 'name':
-            //     this.eventEmitter.emit(Event.MARKETER_REQUEST_BUSINESS_NAME);
-            //     break;
-            // case 'ubication':
-            //     this.eventEmitter.emit(Event.MARKETER_REQUEST_BUSINESS_LOCATION);
-            //     break;
-            // case 'qrCode':
-            //     this.eventEmitter.emit(Event.MARKETER_REQUEST_QR_CODE);
-            //     break;
-            // default:
-            //     this.eventEmitter.emit(Event.MARKETER_SUCCESS_CONFIRM)
-            //     break;
-            case 'RUC/DNI':
-                /**
-                 * TODO:
-                 * Acceder a la funcion que verifica la identidad del negocio/persona
-                 * y en base a la respuesta emitir el evento para pedir el nombre del
-                 * negocio en caso exitoso, en caso contrario volver a emitir el evento
-                 * que vuelve a mandar el mensaje, pidiendo el DNI/Ruc
-                 */
-                this.eventEmitter.emit(Event.MARKETER_REQUEST_BUSINESS_NAME, data);
-                break;
-            case '12345':
-                this.eventEmitter.emit(Event.MARKETER_SUCCESS_CONFIRM)
-                break;
-            default:
-                this.eventEmitter.emit(Event.MARKETER_REQUEST_BUSINESS_LOCATION, data);
-                break;
-        }
-    }
-    
-    public async handleButtonReplyMessage(data: any) {
-        console.info('handler MARKETER_BUTTON__REPLY_MESSAGE');
-
-        const button_id = data.message.button_reply.id
-
-        switch (button_id) {
             case 'step-1-continuar':
-                this.eventEmitter.emit(Event.MARKETER_REQUEST_RUC_DNI, data);
+                await this.marketerEvents.requestRUCDNI(data);
                 break;
             case 'step-1-cancelar':
-                /**
-                 * TODO:
-                 * Emitir el evento que lanza un mensaje de despedida y
-                 * eliminar el registro Marketer de la base de datos
-                 */
-                console.log(button_id);
+                await this.marketerEvents.cancelRegister(data);
+                break;
+            case 'RUC':
+                await this.marketerEvents.validateRUCDNI(data);
+                break;
+            case 'name':
+                await this.marketerEvents.validateName(data);
+                break;
+            case 'ubication':
+                await this.marketerEvents.validateUbication(data);
+                break;
+            case 'image':
+                await this.marketerEvents.validateImage(data);
+                break;
+            case 'qrCode':
+                await this.marketerEvents.validateQrCode(data);
                 break;
             default:
-                console.log('boton desconocido');
+                this.marketerEvents.sendError(data);
+                console.log('case field -> ', req.marketerField);
+                console.log('case no controlado -> ', data);
                 break;
         }
-    }
-
-    public async handleLocationMessage(data:any) {
-        console.log('handler MARKETER_LOCATION_MESSAGE');
-        this.eventEmitter.emit(Event.MARKETER_REQUEST_QR_CODE, data);
     }
 }
