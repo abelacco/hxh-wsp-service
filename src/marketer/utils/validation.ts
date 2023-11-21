4333fe47b8612c7d8d96052dd0660bba8bfbeffc
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-11-20 20:27:12 -0600

    (validations.ts) Add the 'validateLatitudeAndLongitude' function

--- src/marketer/utils/validation.ts
+++ src/marketer/utils/validation.ts
@@ -9,3 +9,16 @@ export function validateDNI(dniNumber: string): boolean {
     const regexDNI = /^[0-9]{8}$/;
     return regexDNI.test(dniNumber);
 }
+
+// Validar valores de Latitud y Longitud
+export function validateLatitudeAndLongitude(latitude: string, longitude: string): boolean {
+    /**
+     * NOTA: No logre encontrar expresiones regulares validas, asi que el metodo no funciona correctamente
+     * Lo dejo por si mas adelante es necesario validar estrictamente, pero por ahora, la misma API de
+     * WhatsApp nos devuelve si si o no ha identificado un mensaje de tipo location
+     */
+    const regexLatitude = /^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/;
+    const regexLongitude = /^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/;
+
+    return regexLatitude.test(latitude) && regexLongitude.test(longitude);
+}
